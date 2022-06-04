c59ca8bdc405001d048b658c1094cb967ccba73f
Gijs Buis <gijsbuis@Gijss-MacBook-Pro.local>
2022-06-04 20:53:32 +0200

    changed bestellingenoverzicht

--- resources/views/drinks.blade.php
+++ resources/views/drinks.blade.php
@@ -4,14 +4,27 @@
 <script src="/drinks.js" defer></script>
 
 <body>
-<h1>Bar overzicht</h1>
-    <?php foreach ($drinks as $drink): ?>
-        <div class="bestelling">
-            <h5>Bestel id: <?= $drink->id ?></h5>
-            <p>Naam: <?= $drink->drank ?></p>
-            <p>Categorie: <?= $drink->categorie ?></p>
-            <p>Besteld door: <?= $drink->besteld_door ?></p>
-            <p>Status: <?= $drink->status?></p>
-        </div>
-    <?php endforeach; ?>
+    <h1>Bar overzicht</h1>
+    <nav class="navigation">
+        <a class="nav_link" href="/drinks">Overzicht bestellingen</a>
+        <a class="nav_link" href="/klanten_overzicht">Overzicht klanten</a>
+    </nav>
+    <section class="bestellingen">
+        <table class="bestelling_table">
+            <tr class="bestelling_row">
+                <th class="bestelling_cell bestelling_header">Bestel ID</th>
+                <th class="bestelling_cell bestelling_header">Klantnaam</th>
+                <th class="bestelling_cell bestelling_header">Bestelling</th>
+                <th class="bestelling_cell bestelling_header">Status</th>
+            </tr>
+            <?php foreach ($drinks as $drink): ?>
+                <tr class="bestelling_row">
+                    <td class="bestelling_cell"><?=$drink->id ?></td>
+                    <td class="bestelling_cell"><?=$drink->besteld_door ?></td>
+                    <td class="bestelling_cell"><?=$drink->drank ?></td>
+                    <td class="bestelling_cell"><?=$drink->status?></td>
+                </tr>
+            <?php endforeach; ?>
+        </table>
+    </section>
 </body>
