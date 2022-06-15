07d3c4209bc84116dc0d34b68506d161ba5248b8
Gijs Buis <gijsbuis@dhcp-145.101.89.162.pool.hsleiden.nl>
2022-06-15 10:01:10 +0200

    changed database column to match Unity request

--- database/migrations/2022_04_30_235709_create_orders_table.php
+++ database/migrations/2022_04_30_235709_create_orders_table.php
@@ -18,7 +18,7 @@ class CreateOrdersTable extends Migration
             $table->string("drank");
             $table->string("categorie");
             $table->integer("aantal");
-            $table->string("aantal_en_drank");
+            $table->string("bestelling");
             $table->integer("user_id")->unsigned();
             $table->foreign("user_id")->references("id")->on("users")->onDelete('restrict');
             $table->string('besteld_door');
