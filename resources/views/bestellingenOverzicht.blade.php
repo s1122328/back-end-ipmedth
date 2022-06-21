af0329181a70862cac01dbbc110c4f7b810015de
Gijs Buis <gijsbuis@Gijss-MacBook-Pro.local>
2022-06-21 16:40:58 +0200

    finalised styling according to Effenaar.nl

--- resources/views/bestellingenOverzicht.blade.php
+++ resources/views/bestellingenOverzicht.blade.php
@@ -25,7 +25,11 @@
             <td class="bestelling_cell"><?=$drink->besteld_door ?></td>
             <td class="bestelling_cell"><?=$drink->aantal?>x <?=$drink->drank ?></td>
             <!-- <td class="bestelling_cell"><?=$drink->status?></td> -->
-            <td class="bestelling_cell"><a class="status_bewerken" href={{"bewerkStatus/".$drink['bestel_id']}}><?=$drink->status?></a></td>
+            <?php if ($drink->status != 'Onderweg'): ?>
+                <td class="bestelling_cell"><a class="status_bewerken" href={{"bewerkStatus/".$drink['bestel_id']}}><?=$drink->status?></a></td>
+            <?php else: ?>
+                <td class="bestelling_cell"><?=$drink->status?></td>
+            <?php endif; ?>
         </tr>
         <?php endforeach; ?>
     </table>
