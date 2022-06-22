8f9d571746e4b2892546fae06b0f2db2fdee9eaa
Gijs Buis <gijsbuis@dhcp-145.101.89.200.pool.hsleiden.nl>
2022-06-22 11:34:34 +0200

    changed edit_bril_id page styling

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -9,12 +9,18 @@
     <script src="/drinks.js" defer></script>
 </head>
 <body>
-    <h1>Bewerk gebruiker</h1>
-    <p>Bril ID bewerken voor gebruiker: {{$user[0]['name']}}</p>
-    <form action="/edit" method="POST">
-        @csrf
-        <input type="hidden" name="id" value="{{$user[0]['id']}}">
-        <input type="text" name="bril_id" value="{{$user[0]['bril_id']}}">
-        <button type="submit">Bewerk bril ID</button>
-    </form>
+    <br>
+    <a class="back_button back_button_margin" href="/koppel">< Ga terug</a>
+    <section class="edit_id">
+        <div class="container">
+            <h1>Koppel bril aan gebruiker</h1>
+            <p>Bril ID bewerken voor gebruiker: {{$user[0]['name']}}</p>
+            <form action="/edit" method="POST">
+                @csrf
+                <input type="hidden" name="id" value="{{$user[0]['id']}}">
+                <input type="text" name="bril_id" value="{{$user[0]['bril_id']}}">
+                <button type="submit">Bewerk bril ID</button>
+            </form>
+        </div>
+        </section>
 </body>
